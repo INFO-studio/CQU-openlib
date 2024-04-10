170563057d0494f6cc4d2351a80c589221c6bdee
saladday <saladay@foxmail.com>
2024-04-10 14:40:11 +0800

    delete .DS_Store /site && update gitignore

--- site/javascripts/baiduStat.js
+++
@@ -1,7 +0,0 @@
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?f482549970210d8b717561682e633c29";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
