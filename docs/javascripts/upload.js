ad44337c20353127f02315bb06608aee27f47d2e
INFOstudio <beta-s@outlook.com>
2024-11-14 17:07:53 +0800

    更新

--- docs/javascripts/upload.js
+++ docs/javascripts/upload.js
@@ -1,13 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
-    if (window.location.pathname.includes("/skills/")) {
+    if (window.location.pathname.includes("/course/")) {
         const feedbackForm = document.querySelector('form.md-feedback[name="feedback"]');
-        
-        // 如果找到反馈表单，则在它前面插入一行小字
         if (feedbackForm) {
-            const contactText = document.createElement("p");
-            contactText.textContent = "You can contact me by email: aaa@bbb.com";
-            contactText.style.fontSize = "small"; // 设置字体大小为小
-            feedbackForm.parentNode.insertBefore(contactText, feedbackForm);
+            const uploadFileTrigger = document.createElement("p");
+            uploadFileTrigger.innerHTML = '<br>如您有本站未收录的文件，欢迎通过<a href="https://forms.office.com/Pages/ResponsePage.aspx?id=DQSIkWdsW0yxEjajBLZtrQAAAAAAAAAAAAMAAA7OwxpURE8xNTROTVRBQTc3M0tDTThaWTVQOENaRC4u">表单</a>来上传文件';
+            uploadFileTrigger.style.textAlign = "center";
+            feedbackForm.parentNode.insertBefore(uploadFileTrigger, feedbackForm);
         }
     }
 });
